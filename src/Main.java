3def63a06529332b0aab9d51c354de5561384f2d
Brad White <white.brad17@gmail.com>
2017-01-25 11:47:53 -0700

    Read in dump file with UTF8 Buffer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,49 @@
+// CC Information starts with %B
+
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    static List<String> lines = new ArrayList<>();
+    static String dumpPath = "memorydump.dmp";
+
+    public static void main(String[] args) throws IOException {
+        /*for(String line : Files.readAllLines(Paths.get(dumpPath))){
+	        lines.add(line);
+        }
+
+	    Scanner fileScanner = new Scanner(new File(dumpPath));
+	    while(fileScanner.hasNext()){
+	        lines.add(fileScanner.next());
+        }
+
+        Iterator iter = lines.listIterator();
+
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }*/
+        try {
+            File fileDir = new File(dumpPath);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(fileDir), "UTF8"));
+
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                System.out.println(str);
+            }
+
+            in.close();
+        } catch (UnsupportedEncodingException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
