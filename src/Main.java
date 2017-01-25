9dd2eca4c87f29898a7b3daa23d18dfb461174e7
BradWhite <white.brad17@gmail.com>
2017-01-25 15:22:18 -0700

    Added CCinfo class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 // CC Information starts with %B
+// TODO handle CL arguments
 
 import java.io.*;
 import java.util.*;
@@ -7,22 +8,9 @@ public class Main {
 
     static List<String> lines = new ArrayList<>();
     static String dumpPath = "memorydump.dmp";
+    static List<CCinfo> foundCCinfo = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        /*for(String line : Files.readAllLines(Paths.get(dumpPath))){
-	        lines.add(line);
-        }
-
-	    Scanner fileScanner = new Scanner(new File(dumpPath));
-	    while(fileScanner.hasNext()){
-	        lines.add(fileScanner.next());
-        }
-
-        Iterator iter = lines.listIterator();
-
-        while(iter.hasNext()){
-            System.out.println(iter.next());
-        }*/
         try {
             File fileDir = new File(dumpPath);
 
