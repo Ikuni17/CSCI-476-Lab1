9eb10a473fcf3359941aff3747649d6cd325e628
BradWhite <white.brad17@gmail.com>
2017-01-25 17:53:05 -0700

    Working regex and printing track 1 information

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    static List<String> lines = new ArrayList<>();
+    //static List<String> lines = new ArrayList<>();
     static String dumpPath = "memorydump.dmp";
     static List<Track1Info> foundTrack1Info = new ArrayList<>();
+    static List<Track1Info> foundTrack2Info = new ArrayList<>();
+    static Pattern track1Pattern = Pattern.compile("%B\\d{13,19}\\^{1}\\w{2,26}\\/\\w{2,26}\\^{1}\\d{7,}\\?");
+    static Matcher track1Matcher;
+    static Pattern track2Pattern = Pattern.compile(";\\d{13,19}={1}\\d{14,}\\?");
+    static Matcher track2Matcher;
 
     public static void main(String[] args) throws IOException {
         try {
@@ -21,7 +28,19 @@ public class Main {
             String str;
 
             while ((str = in.readLine()) != null) {
-                System.out.println(str);
+                track1Matcher = track1Pattern.matcher(str);
+                track2Matcher = track2Pattern.matcher(str);
+                if(track1Matcher.find()) {
+                    System.out.println(track1Matcher.group());
+                    String matchedString = track1Matcher.group();
+                    String[] split = matchedString.split("\\^{1}");
+                    System.out.println(split[0].substring(2));
+                    foundTrack1Info.add(new Track1Info(split[1].replace("/", " "), split[0].substring(2), split[2].substring(0,4), split[2].substring(4,7)));
+                }
+                if(track2Matcher.find()) {
+                    System.out.println(track2Matcher.group());
+                }
+                //System.out.println(str);
             }
 
             in.close();
@@ -33,7 +52,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        foundTrack1Info.add(new Track1Info("Brad", "123445678","09/2019", 1999, 378));
+        //foundTrack1Info.add(new Track1Info("Brad", "123445678", null, 1999, 378));
         foundTrack1Info.get(0).printTrack1Info();
     }
 }
--- src/Track1Info.java
+++ src/Track1Info.java
@@ -1,13 +1,11 @@
 public class Track1Info {
-    String name, ccNumber, expDate;
-    int pin, cvvNum;
+    String name, ccNumber, expDate, serviceCode;
 
-    Track1Info(String name, String ccNumber, String expDate, int pin, int cvvNum) {
+    Track1Info(String name, String ccNumber, String expDate, String serviceCode) {
         this.name = name;
         this.ccNumber = ccNumber;
         this.expDate = expDate;
-        this.pin = pin;
-        this.cvvNum = cvvNum;
+        this.serviceCode = serviceCode;
     }
 
     public String getName() {
@@ -22,16 +20,12 @@ public class Track1Info {
         return expDate;
     }
 
-    public int getPin() {
-        return pin;
-    }
-
-    public int getCvvNum() {
-        return cvvNum;
+    public String getServiceCode() {
+        return serviceCode;
     }
 
     public void printTrack1Info() {
-        System.out.printf("Cardholder's Name: %s%nCard Number: %s%nExpiration Date: %s%nEncrypted PIN: %d%nCVV Number: %d%n", name, ccNumber, expDate, pin, cvvNum);
+        System.out.printf("Cardholder's Name: %s%nCard Number: %s%nExpiration Date: %s%nService Code: %s%n", name, ccNumber, expDate, serviceCode);
     }
 
 }
